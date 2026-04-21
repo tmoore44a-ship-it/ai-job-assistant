d43fc34b49e2b4d249269f5308f91fb6dc73a0fd
Tim Moore <tmoore44a@gmail.com>
2026-04-20 23:36:23 -0600

    Fix WSGI import for Render deployment

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
-from main import app
+"""
+WSGI entry point for Render / Gunicorn.
 
-if __name__ == "__main__":
-    app.run()
+Important:
+- Our Flask application object lives in main.py
+- Gunicorn uses this file when the start command is: gunicorn wsgi:app
+"""
+
+from main import app
